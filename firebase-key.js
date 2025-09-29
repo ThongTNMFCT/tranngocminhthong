eb45a173dcbf51feee1bb40d3e2d52b5fe89e31f
Trần Ngọc Minh Thông <thongtnmfct31178@gmail.com>
2025-09-29 19:23:57 +0700

    Add files via upload

---
+++ firebase-key.js
@@ -0,0 +1,17 @@
+// firebase-key.js
+
+// 1. Firebase Configuration
+// Dưới đây là thông tin cấu hình cho dự án Firebase của bạn.
+export const firebaseConfig = {
+  apiKey: "AIzaSyAMaF0rJV1x7ke1bt379l0AgzDRl7NkwSU",
+  authDomain: "thongtnm.firebaseapp.com",
+  projectId: "thongtnm",
+  storageBucket: "thongtnm.appspot.com", // Sửa lại đúng tên storage bucket
+  messagingSenderId: "685255534783",
+  appId: "1:685255534783:web:2b99055d86be5e7380be0a"
+};
+
+// 2. ImgBB API Key
+// Hãy thay thế 'YOUR_IMGBB_API_KEY_HERE' bằng API key của bạn từ trang api.imgbb.com
+export const IMGBB_API_KEY = 'cc49b6a38299d2c8b719d23c67b0c6c3';
+
